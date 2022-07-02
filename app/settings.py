5cc46435658a62a11e9d8e759ee9f20d68f7ce2a
tvlemes <contato.thiagolemes@yahoo.com>
2022-07-02 11:13:23 -0300

    alter settings.py

--- app/settings.py
+++ app/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v(f=s66s4=n$2mc#8#7u!ru^ra%w+4#a(pq$561j$6hy+2@h19'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
